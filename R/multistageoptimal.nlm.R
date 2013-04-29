34837f0dd181da0134cc1543fd1991a9fcddde6c
Xuefei Mi <xuefeimi@uni-hohenheim.de>
2013-04-29 00:00:00 +0000

    version 2.0.6

--- R/multistageoptimal.nlm.R
+++ R/multistageoptimal.nlm.R
@@ -1,7 +1,7 @@
+# mutistageoptimal.nlm.R author: xuefei mi, 28-03-2013, for selectiongain package v2.0.2
 
 
 `multistageoptimal.nlm`<-function(N.upper, N.lower=rep(N.fs,length(N.upper)), corr, ini.value=c(c(length(N.upper):1)+Budget/(CostC+sum(CostTv)+1)), Budget, CostC=1, CostTv=rep(1,length(N.upper)), N.fs=1, iterlim=100, alg=GenzBretz())
-
 {
 
 # check, if initial value is greater than budget
@@ -17,71 +17,52 @@
 # this is just a shell function, which fits the requirement of the optimization function
       
    MultiStageOptim<-function(xN,N.upper, N.lower,corr,Budget,CostC,CostTv,N.fs,alg)       
-{       
- length.xN=length(xN)
-      
- cost =  xN[1]*CostC+sum(CostTv*xN)
-  
- if (cost <= Budget && all(xN<=N.upper)&& all(xN>N.lower) && cost)     
-{     
- xN.matrix=embed(c(xN,N.fs),2)
+   {       
+     length.xN=length(xN)     
+     cost =  xN[1]*CostC+sum(CostTv*xN)  
+     if (cost <= Budget && all(xN<=N.upper)&& all(xN>N.lower) && cost)     
+     {     
+       xN.matrix=embed(c(xN,N.fs),2)
 
-    if (all(xN.matrix[,1] < xN.matrix[,2]))
-    {   
-     alpha = xN.matrix[,1]/ xN.matrix[,2]
-  
-     Quantile= multistagetp(alpha, corx=corr[-1,-1], alg=alg)     
-                           
-     output<- -multistagegain(Q=Quantile,corr=corr, alg=alg)
+       if (all(xN.matrix[,1] < xN.matrix[,2]))
+       {   
+         alpha = xN.matrix[,1]/ xN.matrix[,2]
+         Quantile= multistagetp(alpha, corx=corr[-1,-1], alg=alg)                               
+         output<- -multistagegain(Q=Quantile,corr=corr, alg=alg)
      
-     if (cost<=Budget*0.9)
+         if (cost<=Budget*0.9)
+         {
+           output=output*0.8
+         }             
+       }else
+         { 
+          output=0
+         }
+     }else
      {
-       output=output*0.8
+       xN.matrix=embed(c(xN,N.fs),2)
+
+       if (all(xN.matrix[,1] < xN.matrix[,2]))
+       {
+         alpha = xN.matrix[,1]/ xN.matrix[,2]
+         Quantile= multistagetp(alpha, corx=corr[-1,-1], alg=alg)                               
+         output<- -multistagegain(Q=Quantile,corr=corr,alg=alg)
+       }else
+       { 
+         output=0
+       }
+       output = 0
      }
-     
-     
-    }else
-        { 
-         output=0
-        }
-
- 
-}else
- {
- 
-  xN.matrix=embed(c(xN,N.fs),2)
-
-    if (all(xN.matrix[,1] < xN.matrix[,2]))
-    {
-     alpha = xN.matrix[,1]/ xN.matrix[,2]
-     
-     Quantile= multistagetp(alpha, corx=corr[-1,-1], alg=alg)     
-                           
-     output<- -multistagegain(Q=Quantile,corr=corr,alg=alg)
-    }else
-        { 
-         output=0
-        }
-  output = 0
- }
- output
-}
-     
-     
-     
+     output
+   }
+   
      # nlm algorithm
-                                       
-result=nlm(MultiStageOptim,p=ini.value,N.upper=N.upper,N.lower=N.lower,corr=corr,Budget=Budget,CostC=CostC,CostTv=CostTv,N.fs=N.fs,alg=alg,hessian = TRUE,iterlim=iterlim)
+      result<-nlm(MultiStageOptim,p=ini.value,N.upper=N.upper,N.lower=N.lower,corr=corr,Budget=Budget,CostC=CostC,CostTv=CostTv,N.fs=N.fs,alg=alg,hessian = TRUE,iterlim=iterlim)
   
-result.table = array(0,length(N.upper)+1)
-
-sample.size=result$estimate
-
-result.table=c(sample.size, -result$minimum)
- 
-names(result.table)=c(rownames(sample.size, do.NULL = FALSE, prefix = "N"),"value")
-
-result.table
-  
+     result.table = array(0,length(N.upper)+1)
+     sample.size=result$estimate
+     result.table=c(sample.size, -result$minimum)
+     names(result.table)=c(rownames(sample.size, do.NULL = FALSE, prefix = "N"),"value")
+     result.table 
 }
 
--- R/multistagetp.R
+++ R/multistagetp.R
@@ -1,73 +1,71 @@
+# mutistagetp.R author: xuefei mi, 29-03-2013, for selectiongain package v2.0.2
+
 `multistagetp` <-
 function(alpha,corx,alg=GenzBretz())
 {
    corr=corx
-    dim=length(alpha)
+   dim=length(alpha)
     
-   # if alpha =1, there will be a problem, the user should reduce alpha slightly
+# if alpha =1, there will be a problem, the user should reduce alpha slightly
   
-    for (i in 1:dim)
-    {
-        if (alpha[i]==1)
-        {
-           alpha[i]=alpha[i]*0.9999
-        }
+   for (i in 1:dim)
+   {
+     if (alpha[i]==1)
+     {
+       alpha[i]=alpha[i]*0.9999
+     }
     
-    }
+   }
     
- # this is for the root search function   
-    qnormbar<-function(kwanted,koutput,corr,howmanyk,alpha, algorithm = alg)
+# this is for the root search function   
+   qnormbar<-function(kwanted,koutput,corr,howmanyk,alpha, algorithm = alg)
    {   
-        corr2dim=corr[1:howmanyk,1:howmanyk] 
-        
-        tempofk1=koutput[1:c(howmanyk-1)]
-          
-        alphatotal=1
-        for (i in 1:howmanyk)
-        {
-           alphatotal=alphatotal*alpha[i]
-        }
+     corr2dim=corr[1:howmanyk,1:howmanyk]        
+     tempofk1=koutput[1:c(howmanyk-1)]
+     alphatotal=1
+     for (i in 1:howmanyk)
+     {
+       alphatotal=alphatotal*alpha[i]
+     }
 
-        pmvnorm(lower = c(tempofk1,kwanted), upper = rep(Inf,howmanyk), mean = rep(0,howmanyk),
+     pmvnorm(lower = c(tempofk1,kwanted), upper = rep(Inf,howmanyk), mean = rep(0,howmanyk),
                    corr = corr2dim, algorithm = alg) - alphatotal   
    }
      
- # repeat the root search for dim dimensions    
+# repeat the root search for dim dimensions    
      
-    koutput=0
-    for (i in  1: dim ) 
-   {
-    
-         if (i == 1)     
-         {
-           koutput[1]= qnorm(alpha[1],lower.tail = FALSE)[[1]][1]
-  
-         }else
-         {   
-              index1=qnormbar(kwanted=100,koutput,corr,i,alpha, algorithm = alg)
-              index2=qnormbar(kwanted=-100,koutput,corr,i,alpha, algorithm = alg)
+   koutput=0
+   for (i in  1: dim ) 
+   {   
+     if (i == 1)     
+     {
+       koutput[1]= qnorm(alpha[1],lower.tail = FALSE)[[1]][1] 
+     }else
+     {   
+       index1=qnormbar(kwanted=100,koutput,corr,i,alpha, algorithm = alg)
+       index2=qnormbar(kwanted=-100,koutput,corr,i,alpha, algorithm = alg)
               
-              #          kwanted should be -100<=  kwanted <= 100
+# kwanted should be -100<=  kwanted <= 100
               
-             if (index1>0)
-            {
-                koutput[i]=100
-            }else if(index2<0)
-            {
-               koutput[i]=-100
-            }          
-            else
-            {
-             koutput[i]= uniroot(qnormbar,interval=c(-100,100),koutput=koutput,corr,i,alpha)[[1]][1]  
-            } 
-         }
-         if (is.infinite(koutput[i])==TRUE)
-         {
-           koutput[i]=-100
-           # this is for the special case that koutput[1] is INF 
-         }
+       if (index1>0)
+       {
+         koutput[i]=100
+       }else if(index2<0)
+       {
+         koutput[i]=-100
+       }else
+       {
+         koutput[i]= uniroot(qnormbar,interval=c(-100,100),koutput=koutput,corr,i,alpha)[[1]][1]  
+       } 
      }
+     if (is.infinite(koutput[i])==TRUE)
+     {
+         koutput[i]=-100
+         
+# this is for the special case that koutput[1] is INF 
 
-koutput
+     }
+  }
+  koutput
 }
 
